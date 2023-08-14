117ddf9eedd1f6243032fe9aeb6c6c00db9afa30
khaledsaifulla05 <khaledsaifulla2001@gmail.com>
2023-08-14 21:49:31 +0600

    Baap Er Bank Making Done

---
+++ js/login.js
@@ -0,0 +1,33 @@
+//step 1 : add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function () {
+
+    //step 2 : get the email address
+    //always remember to use .value to get text from an input field
+
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+
+    //step 3 : get password
+    //3.a. set id on the html element
+    //3.b. get the element
+    //3.c. get the value from the element
+
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+
+
+    // DO NOT verify  email Password on the client side
+    //step 4 : verify email and password
+
+    if (email === 'khaledsaifulla2001@gmail.com' && password === 'ks2001') {
+        console.log(location.href = "bank.html");
+    }
+    else {
+        alert('Invalid User');
+        console.log(location.href = "index.html");
+    }
+
+    emailField.value = '';
+    passwordField.value = '';
+
+});
---
+++ js/withdraw.js
@@ -0,0 +1,30 @@
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    withdrawField.value = '';
+
+    if(isNaN(newWithdrawAmount)){
+       alert('Please Give Numbers')
+        return;
+    }
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousTotalWithdrawString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousTotalWithdrawString);
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Insufficient Balance');
+        return;
+    }
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+});
